3b06015dcfcd259fac2a39c395c61b512f0406c2
parmindergill526 <41070943+parmindergill526@users.noreply.github.com>
2019-01-17 17:07:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterWeek2ListArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+
+            dogs.Add("Bichon");
+            dogs.Add("Poodle");
+            dogs.Add("Retriever");
+
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + " ");
+            }
+            Console.WriteLine(dogs[2]);
+            Console.ReadLine();
+        }
+    }
+}
